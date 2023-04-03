c880d1662ff13484a83e41eb790167f4351cb88e
Dudalara <eduardawaldemar@hotmail.com>
2023-04-02 21:14:33 -0300

    feature atividade 05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderProjectComponent } from './header-project/header-project.component';
+import { FooterProjectComponent } from './footer-project/footer-project.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderProjectComponent,
+    FooterProjectComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/footer-project/footer-project.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FooterProjectComponent } from './footer-project.component';
+
+describe('FooterProjectComponent', () => {
+  let component: FooterProjectComponent;
+  let fixture: ComponentFixture<FooterProjectComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FooterProjectComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FooterProjectComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
