6c49212b8a979292792a1aedb90363d4c8594006
Dudalara <eduardawaldemar@hotmail.com>
2023-04-16 22:15:00 -0300

    atividade routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderProjectComponent } from './header-project/header-project.component';
 import { FooterProjectComponent } from './footer-project/footer-project.component';
+import { FormComponent } from './form/form.component';
+import { ButtonsComponent } from './buttons/buttons.component';
+import { CreateReportComponent } from './create-report/create-report.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReportingInformationComponent } from './reporting-information/reporting-information.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderProjectComponent,
-    FooterProjectComponent
+    FooterProjectComponent,
+    FormComponent,
+    ButtonsComponent,
+    CreateReportComponent,
+    LoginComponent,
+    DashboardComponent,
+    ReportingInformationComponent
   ],
   imports: [
     BrowserModule,
